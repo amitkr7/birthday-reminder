1a6d8462bbd834d9659dfae9b284769145ceb165
Amit <amit840901@gmail.com>
2021-01-03 03:33:07 +0530

    Final App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import data from './data'
+import List from './List'
 
 function App() {
-  return <div className='App'>Birthday Reminder App</div>
+  const [people, setPeople] = useState(data)
+
+  return (
+    <main>
+      <section className='container'>
+        <h3>{people.length} Birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>Clear All</button>
+      </section>
+    </main>
+  )
 }
 
 export default App
